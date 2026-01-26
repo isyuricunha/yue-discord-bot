04c3dfea6572cb58851335c58929171172cda6d8
Yuri Cunha <isyuricunha@duck.com>
2026-01-26 23:22:21 +0000

    chore(web): remove unused service worker

--- apps/web/public/sw.js
+++
@@ -1,8 +0,0 @@
-// Service Worker vazio - desabilitado
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', () => {
-  return self.clients.claim();
-});
