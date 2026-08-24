3b380b8b18d5eff87b477d15af74ff3994352def
Yuri <isyuricunha@duck.com>
2026-08-24 11:23:54 -0300

    fix(web): reuse shared query client

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
+import { queryClient } from './lib/query-client'
 import './index.css'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 1,
-    },
-  },
-})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
