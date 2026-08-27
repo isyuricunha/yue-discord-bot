3465596a6939b03cd1a3a2792487d91454e211f6
Yuri <isyuricunha@duck.com>
2026-08-27 17:41:56 -0300

    chore(logging): pretty-print bot logs by default

--- apps/bot/src/utils/logger.ts
+++ apps/bot/src/utils/logger.ts
@@ -1,20 +1,23 @@
 import pino from 'pino';
 import { CONFIG } from '../config';
 
+const pretty_transport = CONFIG.logFormat === 'pretty'
+  ? {
+      target: 'pino-pretty',
+      options: {
+        colorize: Boolean(process.stdout.isTTY),
+        translateTime: 'SYS:HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{msg}',
+      },
+    }
+  : undefined;
+
 export const logger = pino({
   level: CONFIG.logLevel,
   serializers: {
     err: pino.stdSerializers.err,
     error: pino.stdSerializers.err,
   },
-  transport: CONFIG.environment === 'development' 
-    ? {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: 'HH:MM:ss',
-          ignore: 'pid,hostname',
-        },
-      }
-    : undefined,
+  transport: pretty_transport,
 });
